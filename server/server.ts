86d9702194a1351fd55c2bfe603acdb89c70e97e
Manmohan-coder <manmohanj8268@gmail.com>
2026-04-27 12:40:58 +0530

    uri fix

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,6 @@
 import { config } from 'dotenv'; config()
+import dns from "node:dns/promises"
+dns.setServers(["8.8.8.8",'1.1.1.1'])
 import app from './src/app.js';
 
 import { connectDB } from './src/config/db.js';
